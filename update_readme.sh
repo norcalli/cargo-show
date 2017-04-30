5e0baa5491c11871639236863f11a2e38771b552
Greg Guthe <greg.guthe@gmail.com>
2017-04-30 11:22:14 -0400

    update readme template

--- update_readme.sh
+++ update_readme.sh
@@ -14,47 +14,47 @@ Prints package metadata like pip show, apt-cache show, npm view, gem query, etc.
 
 To install:
 
-\`\`\`
+\`\`\`sh
 \$ cargo install cargo-show
     Updating registry \`https://github.com/rust-lang/crates.io-index\`
-  Installing cargo-show v0.3.0
-   Compiling glob v0.2.11
-   Compiling num-traits v0.1.37
-   Compiling winapi-build v0.1.1
+ Downloading cargo-show v0.4.0
+  Installing cargo-show v0.4.0
+   Compiling utf8-ranges v1.0.0
+   Compiling strsim v0.6.0
+   Compiling libc v0.2.22
 ...
 
-   Compiling cargo v0.17.0
-   Compiling docopt v0.7.0
-   Compiling cargo-show v0.3.0
-    Finished release [optimized] target(s) in 312.4 secs
+   Compiling g-k-crates-io-client v0.8.1
+   Compiling cargo-show v0.4.0
+    Finished release [optimized] target(s) in 94.37 secs
   Installing /Users/greg/.cargo/bin/cargo-show
 \$
 \`\`\`
 
 Usage:
 
-\`\`\`
+\`\`\`sh
 \$ cargo show --help
 $(cargo show --help)
 \`\`\`
 
 To print package metadata:
 
-\`\`\`
+\`\`\`sh
 \$ cargo show webrender servo
 $(cargo show webrender servo 2>&1)
 \`\`\`
 
 To print JSON:
 
-\`\`\`
+\`\`\`json
 \$ cargo show --json serde | cut -b '1-120'
 $(cargo show --json serde | cut -b '1-120')
 \`\`\`
 
 To rename the command if you're used to other package managers:
 
-\`\`\`
+\`\`\`sh
 \$ cd /usr/local/bin/  # or someplace in path
 \$ ln \$(which cargo-show) cargo-flizblorp  # needs to be a hardlink
 \$ cargo --list | grep fliz
